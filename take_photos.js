e261a6b14b44ca9ec1db3a152659f252da7ccf31
Daniel Cioata <dcioata@cmedtechnology.com>
2018-05-12 11:08:16 +0300

    working version

--- take_photos.js
+++ take_photos.js
@@ -1,33 +1,31 @@
-var is_webkit = false;
+'use strict';
+var video = document.querySelector('video');
+var canvas;
 
-function onSuccess(stream) {
-  var video = document.getElementById("v");
-  var canvas = document.getElementById("c");
-  var button = document.getElementById("b");
-  //video.src = is_webkit ? window.webkitURL.createObjectURL(stream) : stream;
-  video.src = stream;
-  button.disabled = false;
-  button.onclick = function() {
-    canvas.getContext("2d").drawImage(video, 0, 0, 300, 300, 0, 0, 300, 300);
-    var img = canvas.toDataURL("image/png");
-    alert("done");
-  };
+function takeSnapshot() {
+  var img = document.querySelector('img') || document.createElement('img');
+  var context;
+  var width = video.offsetWidth
+  , height = video.offsetHeight;
+
+  canvas = canvas || document.createElement('canvas');
+  canvas.width = width;
+  canvas.height = height;
+
+  context = canvas.getContext('2d');
+  context.drawImage(video, 0, 0, width, height);
+
+  img.src = canvas.toDataURL('image/png');
+  document.body.appendChild(img);
 }
 
-function onError(err) {
-  alert("there was an error " + err)
-}
-
-
-if (navigator.getUserMedia) {
-    //opera
-    navigator.getUserMedia({video: true, audio: false}, onSuccess, onError);
-}
-else if (navigator.webkitGetUserMedia) {
-    //webkit users
-    is_webkit = true;
-    navigator.webkitGetUserMedia('video, audio', onSuccess, onError);
-}
-else {
-    //fallback code goes here
+if (navigator.mediaDevices) {
+  navigator.mediaDevices.getUserMedia({video: true})
+    .then(function(stream) {
+      video.src = window.URL.createObjectURL(stream);
+      video.addEventListener('click', takeSnapshot);
+    })
+    .catch(function(error) {
+      document.body.textContent = 'Could not access the camera. Error: ' + error.name;
+    });
 }
