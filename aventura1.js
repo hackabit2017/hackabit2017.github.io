94d10a7a412a1dc5760a94823c86dcf23ace9196
Madincea Vasile <vmadincea@gmail.com>
2018-05-13 12:02:48 +0300

    smoother score...

--- aventura1.js
+++ aventura1.js
@@ -3,6 +3,7 @@ const aventura1 = {
   totalTries: 5,
   tries: 0,
   score: 0,
+  colors: ['#e31d28', '#fcd234', '#3e9965', '#295167', '#492245'],
   currentNumber: null,
   getNextNumber: function() {
     return randint(this.number_limit)
@@ -12,7 +13,9 @@ const aventura1 = {
     say('welcome_message__aventura1')
     setTimeout(function() {say('aventura1__next_number')}, 6000);
     this.currentNumber = this.getNextNumber();
-    domElements.content_aventura.innerHTML = `<div>${this.currentNumber}</div>`;
+    const color = this.colors[randint(4)]
+    console.log('color: ', color)
+    domElements.content_aventura.innerHTML = `<div style="color: ${color}">${this.currentNumber}</div>`;
   },
   onExit: function() {
     console.log('exiting aventura1');
@@ -34,7 +37,7 @@ const aventura1 = {
         this.currentNumber = this.getNextNumber();
         domElements.content_aventura.innerHTML = `<div>${this.currentNumber}</div>`
       } else {
-        domElements.content_aventura.innerHTML = `<p>SCOR: ${this.score}/${this.totalTries}</p><div>Adventure is over. Returning to main menu</div>`;
+        domElements.content_aventura.innerHTML = `<p class="scor">SCOR: ${this.score}/${this.totalTries}</p>`;
         say(thingsToSay.FINISH);
         setTimeout(function(){onCommandReceived('exit')}, 3000)
       }
@@ -42,7 +45,7 @@ const aventura1 = {
       say(thingsToSay.WRONG_ANSWER);
       this.tries += 1;
       this.currentNumber = this.getNextNumber();
-      domElements.content_aventura.innerHTML += `<div>${this.currentNumber}</div>`
+      domElements.content_aventura.innerHTML = `<div>${this.currentNumber}</div>`
     }
   }
 }
